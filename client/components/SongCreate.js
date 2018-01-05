1430b3d759da5f020df4a03842e779877dcd12be
zorail <rohan.seth497@gmail.com>
2018-01-06 00:33:32 +0530

    Mutation done by adding song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongCreate extends Component{
   constructor(props){
@@ -6,11 +8,22 @@ class SongCreate extends Component{
 
     this.state = { title: ''};
   }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
+  }
+
   render(){
     return(
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={e => this.setState({title: e.target.value})}
@@ -22,4 +35,12 @@ class SongCreate extends Component{
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String){
+    addSong(title: $title){
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
