f4d749a1fbcd84ba4eb15abaf8eedd65612e1357
zorail <rohan.seth497@gmail.com>
2018-01-05 22:48:22 +0530

    Rendered data from graphql server

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,22 +1,42 @@
 import React,{ Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component{
+
+    renderSongs(){
+      return this.props.data.songs.map(song => {
+        return (
+            <li key={song.id}>
+              {song.title}
+            </li>
+        );
+      });
+    }
+
     render(){
+      // console.log(this.props);
+      if(this.props.data.loading){
+          return <div>Loading...</div>
+      }
+
       return(
         <div>
-          SongList
+          {this.renderSongs()}
         </div>
       );
     }
 }
 
+//query doesn't get executed here
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `;
 
-export default SongList;
+//graphql(query) returns a function which is immediately invoked by SongList
+export default graphql(query)(SongList);
