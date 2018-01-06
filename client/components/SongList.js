ccfee1999ca3a6442370b93ea635d39d0ed2dfef
zorail <rohan.seth497@gmail.com>
2018-01-06 15:32:15 +0530

    routing for detail component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -17,7 +17,7 @@ class SongList extends Component{
       return this.props.data.songs.map(song => {
         return (
             <li key={song.id} className="collection-item">
-              {song.title}
+              <Link to={`/song/${song.id}`}>{song.title}</Link>
               <i className="material-icons"
                   onClick={() => this.onSongDelete(song.id)}
                 >
